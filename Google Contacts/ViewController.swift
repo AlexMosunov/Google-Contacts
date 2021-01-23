7173ebc4e3b0ad50e874420fa4666174beae337c
Oleksii Mosunov <alexmosunov@gmail.com>
2021-01-23 10:59:32 +0200

    Initial Commit

---
+++ Google Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Google Contacts
+//
+//  Created by Alex Mosunov on 23.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
